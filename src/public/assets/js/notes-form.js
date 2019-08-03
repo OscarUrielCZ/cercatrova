7ef652c05284cf86b0e3e62caa31990b7022ff2c
OscarUrielCZ <oscar2410.oucz@gmail.com>
2019-08-03 09:36:08 -0500

    notifications are ready

--- src/public/assets/js/notes-form.js
+++ src/public/assets/js/notes-form.js
@@ -23,32 +23,57 @@ document.addEventListener('DOMContentLoaded', function() {
 				.then(resp => resp.json())
 				.then(resp => {
 					form.reset();
-					createNotification('Note created successfully', 'Go back', '/', 'page-color');
+					createNotification('Exito', `La nota ${resp.note.title} fue creada`);
 				})
 				.catch(err => {
 					console.log(err);
-					createNotification('Something went wrong', 'Go back', '/', 'page-red');
+					createNotification('Error', 'Algo salió mal, intenta de nuevo');
 				});
 		}
 	});
-	$('.toast').toast('show')
-	$('.toast').toast({ data-autohide: "false" })
 });
 
-createNotification = (text, linkText, link, bgColor) => {
-	let div = document.createElement('div');
-	let a = document.createElement('a')
-	let textNode = document.createTextNode(text);
-	let linkTextNode = document.createTextNode(linkText);
+/*
+		
+*/
 
-	a.href = link;
-	a.className = 'link-notification';
-	a.appendChild(linkTextNode);
+createNotification = (title, text) => {
+	let toast = document.createElement('div');
+	let toastHeader = document.createElement('div');
+	let toastBody = document.createElement('div');
+	let strong = document.createElement('strong');
+	let ago = document.createElement('small');
+	let x = document.createElement('button');
+	let times = document.createElement('span');
 
-	div.className = 'notification ' + bgColor;
-	div.style = 'opacity: 0.7';
-	div.appendChild(textNode);
-	div.appendChild(a);
+	toast.setAttribute('class', 'toast');
+	toast.setAttribute('role', 'alert');
+	toast.setAttribute('aria-live', 'assertive');
+	toast.setAttribute('aria-atomic', 'true');
+	toast.setAttribute('data-autohide', 'false');
 
-	document.querySelector('.container').prepend(div);
-}
+	toastHeader.setAttribute('class', 'toast-header');
+
+	strong.setAttribute('class', 'mr-auto');
+	strong.innerHTML = title;
+	ago.innerHTML = 'hace un momento';
+	x.setAttribute('type', 'button');
+	x.setAttribute('class', 'ml-2 mb-1 close');
+	x.setAttribute('data-dismiss', 'toast');
+	x.setAttribute('aria-label', 'Close');
+	times.setAttribute('aria-hidden', 'true');
+	times.innerHTML = '&times;';
+
+	toastBody.setAttribute('class', 'toast-body');
+	toastBody.innerHTML = text;
+
+	x.appendChild(times);	
+	toastHeader.appendChild(strong);
+	toastHeader.appendChild(ago);
+	toastHeader.appendChild(x);
+	toast.appendChild(toastHeader);
+	toast.appendChild(toastBody);
+
+	document.querySelector('.notifications').appendChild(toast);
+	$('.toast').toast('show');
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -110,8 +110,6 @@ router.get('/create-note', (req, res) => {
 });
 
 router.post('/create-note', async(req, res) => {
-	console.log(req.body);
-
 	let note = new Note({
 		title: req.body.title,
 		category: req.body.category,
